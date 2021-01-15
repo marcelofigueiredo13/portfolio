c06a3a81fd58538ca3dc5842912ed82e6e57f04b
MarceloFigueiredo99 <64539363+MarceloFigueiredo99@users.noreply.github.com>
2021-01-15 00:23:03 +0000

    Add files via upload

---
+++ Software(C++)/Face.cpp
@@ -0,0 +1,16 @@
+#include "Face.h"
+
+Face::Face(int _V1,int _V2,int _V3)
+{
+    //cout << "Passei em [" << __FUNCTION__ << "]" <<endl;
+    V1 = _V1;
+    V2 = _V2;
+    V3 = _V3;
+}
+
+Face::~Face()
+{
+    //cout << "Passei em [" << __FUNCTION__ << "]" <<endl;
+}
+
+
---
+++ Software(C++)/Vertice.cpp
@@ -0,0 +1,18 @@
+#include "Vertice.h"
+
+Vertice::Vertice(float _x,float _y,float _z)
+{
+    //cout << "Passei em [" << __FUNCTION__ << "]" <<endl;
+    x = _x;
+    y = _y;
+    z = _z;
+}
+
+Vertice::~Vertice()
+{
+    //cout << "Passei em [" << __FUNCTION__ << "]" <<endl;
+}
+
+
+
+
