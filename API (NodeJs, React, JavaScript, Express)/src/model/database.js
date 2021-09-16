6c8d30c3e8529fb559bf35f200d54cf777de5c11
MarceloFigueiredo99 <64539363+MarceloFigueiredo99@users.noreply.github.com>
2021-09-16 20:38:21 +0100

    Update database.js

--- API (NodeJs, React, JavaScript, Express)/src/model/database.js
+++ API (NodeJs, React, JavaScript, Express)/src/model/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-'da00p68s3m0d9g', //nome da base de dados
-'ysyljumnfpuxpr', //nome do utilizador
-'d5f7df9f0de11df38b9865dd7073808b62a6bd1ecd2af0e769b4f87f6b50c450', //password
+'xxx', //nome da base de dados
+'xxx', //nome do utilizador
+'xxx', //password
 {
-host: 'ec2-54-216-48-43.eu-west-1.compute.amazonaws.com',
+host: 'xxx',
 port: '5432',
 dialect: 'postgres',
 ssl:true,
